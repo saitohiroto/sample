71ed6138ef1b656d1ab1de6c275e2e7746bf478f
齊藤寛人 <hiroto@saitouhiroto-no-MacBook-Pro.local>
2016-06-30 00:04:37 +0900

    初コミット

---
+++ EX-11 -BMIHanteiHanpuku(ForINCL).swift
@@ -0,0 +1,18 @@
+//
+//  EX-11 -BMIHanteiHanpuku(ForINCL).swift
+//  
+//
+//  Created by 齊藤寛人 on 2016/06/21.
+//
+//
+
+import Foundation
+
+var sintai = [(180.0,88.8),(175.5,58.2),(170.8,65.7),(184.6,105.5),(165.3,50.0)]
+
+var sintyou:Double
+var taijyuu:Double
+var hyoutai:Double
+var bmi:Double
+
+for wsintai in 
